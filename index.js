95de6285056ed92be26fe8e4be99df7005b30bd0
Thomas Kinsella <thomasjkinsella4@gmail.com>
2022-02-25 13:10:06 -0500

    commit tree 2cfb7d3b4f48e45f9a35012f28d21ec3f6f8449e

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // Code your solution here
+
+function findMatching(array, string) {
+     let result = array.filter((str) => {
+        return str.toUpperCase() === string.toUpperCase();
+    });
+    return result;
+};
+
+function fuzzyMatch(array, string) {
+    let result = array.filter((str) => {
+        return str.startsWith(string) === string.startsWith(string);
+    });
+    return result;
+}
+
+function matchName(array, string) {
+    let result = array.filter((str) => {
+        return str.name === string;
+    } );
+    return result;
+}
+
+
+
+
